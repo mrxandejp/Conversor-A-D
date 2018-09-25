2f3a6507367fcb662e89380232b48abebb6b8ed3
mrxandejp <mrxandejp@gmail.com>
2018-09-24 22:17:51 -0300

    Conversor A/D: medição de tensão e indicação em % de 5V

---
+++ build/default/debug/Alexandre-Conversor.o.d
@@ -0,0 +1,4 @@
+build/default/debug/Alexandre-Conversor.o:  \
+	/home/alexandre/MPLABXProjects/Atividade3-AV2.X/Alexandre-Conversor.ASM  \
+	/opt/microchip/mplabx/v4.10/mpasmx/p12f675.inc  \
+
---
+++ build/default/production/Alexandre-Conversor.o.d
@@ -0,0 +1,3 @@
+build/default/production/Alexandre-Conversor.o:  \
+	/home/alexandre/MPLABXProjects/Atividade3-AV2.X/Alexandre-Conversor.ASM  \
+
